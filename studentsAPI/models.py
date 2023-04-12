b9eabd1d1fce22419e4e67ef2c9b29e0d36d7ed4
Balqees Al Jabri <balqeesaljabri88@gmail.com>
2023-04-13 01:17:41 +0400

    fourth commit create models for entities and add fields to each enitity

--- studentsAPI/models.py
+++ studentsAPI/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+# creating the entities of our tables followed with their fields 
+
+''' I then showcased the relationship between 
+    (student and class) 
+        and between
+     (student and country)
+    where it is many to one by defining the 'ForeignKey' field type
+    in the student model 
+      '''
+# Automatically generate CreatedDate and ModifiedDate properties for all entities
+class Class(models.Model):
+    class_name = models.CharField(max_length=50)
+    createdDate = models.DateTimeField(auto_now_add=True, null=True)
+    modifiedDate = models.DateTimeField(auto_now=True, null=True)
+
+    def __str__(self):
+        return self.class_name
+
+class Country(models.Model):
+    name = models.CharField(max_length=50, null=True)
+    createdDate = models.DateTimeField(auto_now_add=True, null=True)
+    modifiedDate = models.DateTimeField(auto_now=True, null=True)
+    def __str__(self):
+        return self.name
+    
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+    date_of_birth = models.DateField(null=True)
+    mobile = models.IntegerField(null=True)
+    createdDate = models.DateTimeField(auto_now_add=True, null=True)
+    modifiedDate = models.DateTimeField(auto_now=True, null=True)
+    
+    class_id = models.ForeignKey(Class, null=True, on_delete=models.SET_NULL)
+    country_id = models.ForeignKey(Country, null=True,  on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
+   
+
