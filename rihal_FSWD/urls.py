084505038cbfededdff39e9d0fbd4b67bc724089
Balqees Al Jabri <balqeesaljabri88@gmail.com>
2023-04-17 02:49:30 +0400

    *modified urls.py to empty path

--- rihal_FSWD/urls.py
+++ rihal_FSWD/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('student/', include('studentsAPI.urls'))
+    path(' ', include('studentsAPI.urls'))
 
 ]
