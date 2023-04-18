f3abfb52a47801a948743f714c24dada61d8fee0
Balqees Al Jabri <balqeesaljabri88@gmail.com>
2023-04-19 00:17:20 +0400

    *undo collectstatic command

--- staticfiles/js/statistics.ae67bffbf69b.js
+++
@@ -1,6 +0,0 @@
-// JS code for toggling dark/light mode
-let toggleDark = document.querySelector("#toggleDark");
-
-toggleDark.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
-});
